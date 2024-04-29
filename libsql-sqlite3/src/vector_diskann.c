penberg/libsql

commit e44a31e5062d885927247254f8c2b19f9155c92a

sqlite3, diskann: Add assertion to neighbourMetadataOffset()

libsql-sqlite3/src/vector_diskann.c
@@ -141,6 +141,7 @@ static int neighbourMetadataOffset(DiskAnnIndex *pIndex){
   nVectorSize = vectorSize(pIndex);
   nNeighbourVectorSize = vectorSize(pIndex);
   maxNeighbours = (nBlockSize - nVectorSize - VECTOR_METADATA_SIZE) / (nNeighbourVectorSize + NEIGHBOUR_METADATA_SIZE);
+  assert( maxNeigbours > 0);
   return nVectorSize + VECTOR_METADATA_SIZE + maxNeighbours * (nNeighbourVectorSize); 
 }
 
@@ -257,10 +258,10 @@ static int diskAnnWriteVector(
   /* nNeighbours */
   blockData[off++] = nNeighbours;
   blockData[off++] = nNeighbours >> 8;
-  off += vectorSerializeToBlob(pVec, (void*)blockData+off, DISKANN_BLOCK_SIZE);
-  for (int i = 0; i < nNeighbours; i++) {
-    off += vectorSerializeToBlob(aNeighbours[i], (void*)blockData+off, DISKANN_BLOCK_SIZE);
-  }
+//  off += vectorSerializeToBlob(pVec, (void*)blockData+off, DISKANN_BLOCK_SIZE);
+//  for (int i = 0; i < nNeighbours; i++) {
+//    off += vectorSerializeToBlob(aNeighbours[i], (void*)blockData+off, DISKANN_BLOCK_SIZE);
+//  }
   off = neighbourMetadataOffset(pIndex);
   for( int i = 0; i < nNeighbours; i++ ){
     blockData[off++] = aNeighbourMetadata[i].id;

commit a8a12babc0869fafaf87a8323d8e19ca18ca70db

sqlite3, diskann: Fix vector block neighbour serialization
Broken by commit e44a31e5062d885927247254f8c2b19f9155c92a ("sqlite3,
diskann: Add assertion to neighbourMetadataOffset()").

libsql-sqlite3/src/vector_diskann.c
@@ -256,10 +256,10 @@ static int diskAnnWriteVector(
   /* nNeighbours */
   blockData[off++] = nNeighbours;
   blockData[off++] = nNeighbours >> 8;
-//  off += vectorSerializeToBlob(pVec, (void*)blockData+off, DISKANN_BLOCK_SIZE);
-//  for (int i = 0; i < nNeighbours; i++) {
-//    off += vectorSerializeToBlob(aNeighbours[i], (void*)blockData+off, DISKANN_BLOCK_SIZE);
-//  }
+  off += vectorSerializeToBlob(pVec, (void*)blockData+off, DISKANN_BLOCK_SIZE);
+  for (int i = 0; i < nNeighbours; i++) {
+    off += vectorSerializeToBlob(aNeighbours[i], (void*)blockData+off, DISKANN_BLOCK_SIZE);
+  }
   off = neighbourMetadataOffset(pIndex);
   for( int i = 0; i < nNeighbours; i++ ){
     blockData[off++] = aNeighbourMetadata[i].id;